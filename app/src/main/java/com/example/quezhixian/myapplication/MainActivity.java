8a4ee6dc0509196b6f5214c0daac80931b988c8e
Fnjcm <609842522@qq.com>
2017-03-24 13:58:56 +0800

    第三次修改mainActivity

--- app/src/main/java/com/example/quezhixian/myapplication/MainActivity.java
+++ app/src/main/java/com/example/quezhixian/myapplication/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
       Log.i("crc","test");
-
+        Log.i("crc","test2");
 
     }
 }
