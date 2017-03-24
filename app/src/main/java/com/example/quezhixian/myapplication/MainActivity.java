b55bc70409b1a0559f821361361c996d9c8de4f7
Fnjcm <609842522@qq.com>
2017-03-24 14:15:06 +0800

    添加abc.java

--- app/src/main/java/com/example/quezhixian/myapplication/MainActivity.java
+++ app/src/main/java/com/example/quezhixian/myapplication/MainActivity.java
@@ -11,8 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      Log.i("crc","test");
-        Log.i("crc","test2");
+        Log.i("crc", "test");
+        Log.i("crc", "test2");
 
     }
 }
---
+++ app/src/main/java/com/example/quezhixian/myapplication/abc.java
@@ -0,0 +1,8 @@
+package com.example.quezhixian.myapplication;
+
+/**
+ * Created by QueZhixian on 2017/3/24.
+ */
+
+public class abc {
+}
