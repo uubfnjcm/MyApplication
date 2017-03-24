401881cd8ced1b8ace0ec55de5be08d8da8759ff
Fnjcm <609842522@qq.com>
2017-03-24 13:57:24 +0800

    修改mainActivity

--- app/src/main/java/com/example/quezhixian/myapplication/MainActivity.java
+++ app/src/main/java/com/example/quezhixian/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.quezhixian.myapplication;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+      Log.i("crc","test");
 
 
     }
